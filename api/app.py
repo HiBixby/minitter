4a8466b47c36a048b95d4fd64093f8fd5ba24829
HiBixby <fpssj@naver.com>
2022-08-16 20:46:47 +0900

    ✨ Add Custom JSON Encoder

--- api/app.py
+++ api/app.py
@@ -1,62 +1,74 @@
 from flask import Flask, request, jsonify
+from flask.json import JSONEncoder
 
-app               = Flask(__name__)
-app.users         = {}
-app.id_count      = 1
-app.tweets        = []
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+        return JSONEncoder.default(self, obj)
+
+
+app.json_encoder = CustomJSONEncoder
+app = Flask(__name__)
+app.users = {}
+app.id_count = 1
+app.tweets = []
 
 
 @app.route("/")
 def index():
-  return "Hello Flask!" 
-  
-@app.route("/ping",methods=['GET'])
-def ping():
-  return "pong"
+    return "Hello Flask!"
 
-@app.route("/sign-up",methods=['POST'])
+
+@app.route("/ping", methods=['GET'])
+def ping():
+    return "pong"
+
+
+@app.route("/sign-up", methods=['POST'])
 def sign_up():
-  new_user                    = request.json
-  new_user["id"]              = app.id_count
-  app.users[app.id_count]     = new_user
-  app.id_count                = app.id_count + 1
-  return jsonify(new_user)
+    new_user = request.json
+    new_user["id"] = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count = app.id_count + 1
+    return jsonify(new_user)
 
 
 @app.route('/tweet', methods=['POST'])
 def tweet():
-  payload=request.json
-  user_id=int(payload['id'])
-  tweet=payload['tweet']
+    payload = request.json
+    user_id = int(payload['id'])
+    tweet = payload['tweet']
 
-  if user_id not in app.users:
-    return '사용자가 존재하지 않습니다',400
+    if user_id not in app.users:
+        return '사용자가 존재하지 않습니다', 400
 
-  if len(tweet)>300:
-    return '300자를 초과했습니다',400
+    if len(tweet) > 300:
+        return '300자를 초과했습니다', 400
 
-  user_id=int(payload['id'])
+    user_id = int(payload['id'])
 
-  app.tweets.append({
-    'user_id':user_id,
-    'tweet': tweet
-  })
-  return '',200
+    app.tweets.append({
+        'user_id': user_id,
+        'tweet': tweet
+    })
+    return '', 200
 
-@app.route('/follow',methods=['POST'])
+
+@app.route('/follow', methods=['POST'])
 def follow():
-	payload			= request.json
-	user_id			= int(payload['id'])
-	user_id_to_follow= int(payload['follow'])
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['follow'])
 
-	if user_id not in app.users or user_id_to_follow not in app.users:
-		return '사용자가 존재하지 않습니다',400
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return '사용자가 존재하지 않습니다', 400
 
-	user = app.users[user_id]
-	user.setdefault('follow',set()).add(user_id_to_follow)
+    user = app.users[user_id]
+    user.setdefault('follow', set()).add(user_id_to_follow)
 
-	return jsonify(user)
-
+    return jsonify(user)
 
 
 app.run(host='0.0.0.0', port=5000)
